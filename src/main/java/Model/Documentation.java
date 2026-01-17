f79f87e2dc181e46dc8a8fd3152351c69f0f47cc
Manfred Armijos <Manfred Armijos@ALMM>
2026-01-17 13:54:19 -0500

    borrando los cambios

--- src/main/java/Model/Documentation.java
+++ src/main/java/Model/Documentation.java
@@ -28,8 +28,5 @@ public class Documentation {
                 .append("passengerCapacity", cruiseShip.getPassengerCapacity()) // Maximum passenger capacity
                 .append("passengers", cruiseShip.getCurrentPassengers());              // Current number of passengers on board
     }
-    
-    public void saludo(){
-        System.out.println("Hello");
-    }
+
 }
